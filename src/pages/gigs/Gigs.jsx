f7c4bfa58640a52213d0a7bf361b06c7eb588ba6
aswin <aswinpalayad@ymail.com>
2023-02-28 20:30:55 +0530

    Gigs page completed

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Gigs.scss"
+import {gigs} from '../../data'
+import GigCard from '../../componets/GigCard'
 
 const Gigs = () => {
+
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState('sales');
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false)
+  }
+
   return (
-    <div>Gigs</div>
+    <div className="gigs">
+      <div className="container">
+          <span className="breadcrumbs">FIVERR > GRAPHICS & DESIGN > </span>
+          <h1>Logo Design</h1>
+          <p>Stand out from the crowd with a logo that fits your brand personality.</p>
+          <div className="menu">
+            <div className="left">
+              <span>Budged</span>
+              <input type="text" placeholder='min' />
+              <input type="text" placeholder='max' />
+              <button>Apply</button>
+            </div>
+            <div className="right">
+                <span className='sortBy' >SortBy</span>
+                <span className="sortType">{sort === 'sales' ? "Best Selling" : "Newest"}</span>
+                <img src="/images/down.png" alt="" onClick={()=> setOpen(!open)} />
+                
+                {open && (
+                  <div className="rightMenu">
+                    {sort === "sales" ? (
+                        <span onClick={()=> reSort('createdAt')} >Newest</span>
+                    ): (
+                      <span onClick={()=> reSort('sales')} >Best Selling</span>
+                    )}
+                  
+                </div>
+                )}
+            </div>
+          </div>
+
+              <div className="cards">
+                {gigs.map(gig => (
+                  <GigCard key={gig.id} item={gig} />
+                ))}
+              </div>
+
+      </div>
+    </div>
   )
 }
 
